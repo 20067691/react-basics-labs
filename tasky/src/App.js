84cd9cbd436f5b5aeba76416241763a35fb7667c
Dean Sinnott <20067691@mail.wit.ie>
2023-10-04 22:31:48 +0100

    added delete and form

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,28 +1,46 @@
 import React, {useState} from 'react';
 import './App.css';
 import Task from './components/Task';
+import AddTaskForm from './components/Form';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" },
-      { id: 4, title: "Lab Work", description: "Do my Web App lab work", deadline: "2 days"},
-      { id: 5, title: "Gym", description: "WHO WILL CARRY THE BOATS", deadline: "Everday"}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false },
+      { id: 4, title: "Lab Work", description: "Do my Web App lab work", deadline: "2 days", done: false},
+      { id: 5, title: "Gym", description: "WHO WILL CARRY THE BOATS", deadline: "Everday", done: false}
     ]
-  });
+  })
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
+
+  const deleteHandler = (taskIndex) => {
+    const tasks =[...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  }
+  ;
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (              
+      {taskState.tasks.map((task, index) => (              
     <Task 
       title={task.title}
       description={task.description}
       deadline={task.deadline}
       key={task.id}
+      done={task.done}
+      markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
     />
   ))}
+  <AddTaskForm />
     </div>
   );
 }
