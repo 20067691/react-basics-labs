f6702c6e0c4a744168f4fec1e2bb6dc558208988
Dean Sinnott <20067691@mail.wit.ie>
2023-10-17 11:22:03 +0100

    Material UI addition

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Task from './components/Task';
 import AddTaskForm from './components/Form';
 import { v4 as uuidv4 } from 'uuid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -68,7 +70,26 @@ function App() {
 ;
   return (
     <div className="container">
-      <h1>Tasky</h1>
+            {/* App Header */}
+            <Container component="main">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          gutterBottom
+          sx = {{
+            backgroundColor: 'gray',
+            textAlign: 'center',
+            color: 'white',
+            padding: '20px',
+            margin: '20px 0 40px 0',
+            borderRadius: '4px'
+          }}
+        >
+          Tasky
+        </Typography>
+      </Container>
+      {/* End App Header */}
       {taskState.tasks.map((task, index) => (              
     <Task 
       title={task.title}
